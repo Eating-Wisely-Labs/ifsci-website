ef0c4ff492c96c2c4c52e1f9444926830bbf2936
Meow Lord <supermeowlord@gmail.com>
2025-01-14 14:29:38 +0800

    feat: WIP

--- src/router.tsx
+++ src/router.tsx
@@ -5,7 +5,6 @@ import HomePage from '@/views/home-page'
 const LinkTwitter = lazy(() => import('@/views/link-twitter'))
 const UserProfile = lazy(() => import('@/views/user-profile'))
 const CheckinSettings = lazy(() => import('@/views/checkin/settings'))
-const UserProfileHome = lazy(() => import('@/views/user-profile-home'))
 const Annotation = lazy(() => import('@/views/annotation'))
 const AnnotationList = lazy(() => import('@/views/annotation-list'))
 const CheckinHome = lazy(() => import('@/views/checkin/home'))
@@ -19,7 +18,6 @@ export default function Router() {
         <Route path="/checkin" element={<CheckinHome></CheckinHome>} />
         <Route path="/profile/:address" element={<UserProfile></UserProfile>} />
         <Route path="/checkin/settings" element={<CheckinSettings></CheckinSettings>} />
-        <Route path="/home/:address" element={<UserProfileHome></UserProfileHome>} />
         <Route path="/annotations/:address" element={<AnnotationList></AnnotationList>} />
         <Route path="/s/:id" element={<Annotation></Annotation>} />
       </Routes>
