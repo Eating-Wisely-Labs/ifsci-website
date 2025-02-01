5579398733748ee44450bd181063c2efc1497fca
missy-wang <sixmonthwang@gmail.com>
2025-02-01 23:43:57 +0800

    feat: modify share on X

--- src/components/user-profile-home/claim-modal.tsx
+++ src/components/user-profile-home/claim-modal.tsx
@@ -1,4 +1,5 @@
-import modalBackgroundSvg from './modal-background.svg'
+// import modalBackgroundSvg from './modal-background.svg'
+import modalBackgroundPng from './claim-modal-bg.png'
 import claimedPng from './claimed.png'
 import { X } from 'lucide-react'
 import twitterXLine from './twitter-x-line.svg'
@@ -13,17 +14,19 @@ export default function Component({ isOpen, points, onClose }: ComponentProps) {
   if (!isOpen) return null
 
   function shareOnX() {
-    const shareText = `I just claimed @${points} IFSCI in the Surprise Airdrop on Intermittent Fasting Science! 🎉 Hurry and click @ifsci_ai on Twitter to learn more and seize this amazing opportunity!`
-    const url = `https://x.com/intent/tweet?text=${encodeURIComponent(shareText)}&url=${encodeURIComponent('https://ifsci.wtf/static/share/twitter-share-landing.html')}`
+    const shareText = `I just claimed ${points} $IFSCI in the Surprise Airdrop on Intermittent Fasting Science! 🎉 Hurry and click @ifsci_ai on Twitter to learn more and seize this amazing opportunity!`
+    const timestamp = Math.floor(Date.now() / 1000)
+    const landingUrl = `https://ifsci.wtf/static/share/twitter-share-landing.html?t=${timestamp}`
+    const url = `https://x.com/intent/tweet?text=${encodeURIComponent(shareText)}&url=${encodeURIComponent(landingUrl)}`
     window.open(url, '_blank')
   }
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
       <div
-        className="relative w-full max-w-[386px] rounded-3xl bg-[#1A1A1A] p-6 text-center"
+        className="relative max-h-[365px] w-full max-w-[386px] rounded-3xl bg-[#1A1A1A] p-6 text-center"
         style={{
-          backgroundImage: `url(${modalBackgroundSvg})`,
+          backgroundImage: `url(${modalBackgroundPng})`,
           backgroundRepeat: 'no-repeat',
           backgroundSize: '100%'
         }}
@@ -32,8 +35,8 @@ export default function Component({ isOpen, points, onClose }: ComponentProps) {
         <button onClick={onClose} className="absolute right-4 top-4 text-white/50 transition-colors hover:text-white">
           <X size={24} />
         </button>
-        <div>
-          <img className="mx-auto size-[180px]" src={claimedPng} alt="" />
+        <div className="mt-[196px]">
+          {/* <img className="mx-auto size-[180px]" src={claimedPng} alt="" /> */}
           {/* <p className="mt-4 text-[26px] leading-[34px] text-white">
             Miss it? New Airdrop <br />
             Season is now underway!
